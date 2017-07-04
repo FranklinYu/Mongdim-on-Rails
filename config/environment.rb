5cf4a51d398a3b25e9cc982fc57ab05bbcc3f07c
Franklin Yu <franklinyu@hotmail.com>
2017-07-04 13:33:59 -0400

    Abort early when secret key base is not found

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require_relative 'application'
 
+# this is required by `./secrets.yml`
+if Rails.application.secrets.secret_key_base.nil?
+  raise 'secret key base not found; specify it in `config/secrets.yml`'
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
